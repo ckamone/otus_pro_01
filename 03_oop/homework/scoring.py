789a21b63c45b2241b3498356d7f17ddb42d28b1
ckamone <ckamone@gmail.com>
2023-05-13 21:16:18 +0300

    fix scoring for pylint

--- 03_oop/homework/scoring.py
+++ 03_oop/homework/scoring.py
@@ -1,7 +1,9 @@
+"""Module with scoring funcs"""
 import random
 
 
-def get_score(store, phone, email, birthday=None, gender=None, first_name=None, last_name=None):
+def get_score(phone, email, birthday=None, gender=None, first_name=None, last_name=None):
+    """Func to score"""
     score = 0
     if phone:
         score += 1.5
@@ -14,6 +16,9 @@ def get_score(store, phone, email, birthday=None, gender=None, first_name=None, 
     return score
 
 
-def get_interests(store, cid):
-    interests = ["cars", "pets", "travel", "hi-tech", "sport", "music", "books", "tv", "cinema", "geek", "otus"]
+def get_interests():
+    """Interests randomizer"""
+    interests = ["cars", "pets", "travel", "hi-tech",
+                 "sport", "music", "books", "tv",
+                 "cinema", "geek", "otus"]
     return random.sample(interests, 2)
